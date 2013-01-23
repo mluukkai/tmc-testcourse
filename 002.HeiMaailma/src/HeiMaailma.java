f02b3c9c63dc38906ac87a164633ecd900f84361
Matti Luukkainen <mluukkai@iki.com>
2013-01-23 22:46:43 +0200

    foobar

---
+++ 002.HeiMaailma/src/HeiMaailma.java
@@ -0,0 +1,12 @@
+public class HeiMaailma {
+
+    public static void main(String[] args) {
+        // Toteuta ohjelmasi tähän.
+
+        // BEGIN SOLUTION
+        System.out.println("Hei Maailma!");
+        System.out.println("(Ja Mualima!)");
+        // END SOLUTION
+    }
+
+}
