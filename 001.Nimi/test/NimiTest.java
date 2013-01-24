b4e5dfa3ea39a9a60d1fb757873aed2cbe312f61
Matti Luukkainen <mluukkai@iki.com>
2013-01-24 03:42:13 +0200

    foobazz

--- 001.Nimi/test/NimiTest.java
+++ 001.Nimi/test/NimiTest.java
@@ -5,15 +5,24 @@ import static org.junit.Assert.*;
 import fi.helsinki.cs.tmc.edutestutils.Points;
 import fi.helsinki.cs.tmc.edutestutils.MockStdio;
 
-@Points("1")
+
 public class NimiTest {
     @Rule
     public MockStdio io = new MockStdio();
 
+	 @Points("1.1")
     @Test
     public void test() {
         Nimi.main(new String[0]);
         String out = io.getSysOut();
         assertTrue("Et tulostanut mitään!",out.length()>0);
+    }
+
+	 @Points("1.2")
+    @Test
+    public void test2() {
+        Nimi.main(new String[0]);
+        String out = io.getSysOut();
+        assertTrue("nimen pituus ei voi olla alle 5",out.length()>5);
     }
 }
